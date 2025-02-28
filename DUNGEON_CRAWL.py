42628e67454f01e15eabc809dd8b8fbc2f8ae8d5
Shawn Doremus <shawn.doremus@gmail.com>
2025-02-28 00:06:17 -0500

    Tried adding class choice - still working on it.

--- DUNGEON_CRAWL.py
+++ DUNGEON_CRAWL.py
@@ -33,7 +33,8 @@ class Player:
         if self.is_alive == True:
             self.is_alive = False
         print("{} has met their early end. Good luck next time.".format(self.name))
-            
+
+#Define the Monster class            
 class Monster:
     def __init__ (self, name, hp, attack, defense):
         self.name = name
@@ -42,17 +43,19 @@ class Monster:
         self.attack = attack
         self.defense = defense
 
+#Define the Item class
 class Item:
     def __init__ (self, name, purpose, modifier):
         self.name = name
         self.purpose = purpose
         self.modifier = modifier
 
+#Define the Dungeon class. (Should the dungeon be a class? I'm not sure.)
 class Dungeon:
     def __init__ (self, length = 5):
         self.length = length
 
-#Introduction to Game
+#Introduction to Game and Player Creation
 print("Greetings, and well met! Welcome to DUNGEON CRAWL. Your task? Survive.")
 
 pc_name = input("First, however, we will need to know a bit more about you. What is your name?\n")
@@ -60,7 +63,7 @@ pc_name = input("First, however, we will need to know a bit more about you. What
 print("Excellent. Welcome to Eosterra, {}.".format(pc_name))
 
 while True:
-    pc_class = input("Now, what class are you? Input '1' for Fighter or '2' for Sorcerer.\n")
+    pc_class = input("Now, what class will you choose? Choose '1' for Fighter or '2' for Sorcerer.\n")
     if pc_class == 1:
         pc_class = "Fighter"
         print("Very well. {} shall be a fighter. Their stats and inventory have been updated to reflect this.")
@@ -68,3 +71,10 @@ while True:
     elif pc_class == 2:
         print("I'm sorry, but sorcerers haven't been invented yet. Please choose another class.")
         continue
+    else:
+        print("I'm sorry, I didn't catch that. Try again.")
+        continue
+    
+print("PAUSE HERE.")
+
+#STOPPED HERE at 12:05am on 2/28/25
