5b6e9533d2b7dc68c599fcb1eaaf423c0580b872
Shawn Doremus <shawn.doremus@gmail.com>
2025-02-28 23:26:14 -0500

    Expanded classes, created the dice roll function, and worked on basic class selection functionality.

--- DUNGEON_CRAWL.py
+++ DUNGEON_CRAWL.py
@@ -4,16 +4,26 @@
 #Import the random module
 import random
 
+#Random Needed Functions??
+def roll_dice(num_sides, quantity):
+    rolls = []
+    total = 0
+    for die in range(quantity):
+        rolls.append(random.randint(1, int(num_sides - 1)))
+    for roll in rolls:
+        total += roll
+    print(f"You rolled {quantity} d{num_sides}. Results: {rolls};{total}")
+        
+    
 #Define Classes
-
 #Define the Player class
 class Player:
     def __init__ (self, name, hp, attack, defense, gold, potions):
         self.name = name
         self.max_hp = hp
         self.hp = self.max_hp
-        self.attack = attack
-        self.defense = defense
+        self.attack_value = attack
+        self.defense_value = defense
         self.gold = gold
         self.potions = potions
         self.is_alive = True
@@ -21,20 +31,31 @@ class Player:
     def __repr__ (self):
         return "Player Info \n Name: {}\n HP: {}/{}\n Attack: {}\n Defense: {}\n Gold: {}\n Potions: {}\n".format (self.name, self.hp, self.max_hp, self.attack, self.defense, self.gold, self.potions)
     
+    def attack(self, opponent):
+        dmg = self.attack
+        opponent.lose_hp(dmg)
+        if opponent.hp == 0:
+            opponent.drop_loot #add this method to the monster class
+    
     def lose_hp(self, dmg):
+        dmg = dmg - self.defense_value
         self.hp -= dmg
         if self.hp <= 0:
             self.hp = 0
             self.is_alive = False
-            self.dead()
+            self.knock_out()
         print("{} takes {} damage. {}'s hp is now {}/{}".format(self.name, dmg, self.name, self.hp, self.max_hp))
     
     def knock_out(self):
         if self.is_alive == True:
             self.is_alive = False
-        print("{} has met their early end. Good luck next time.".format(self.name))
+        print(f"{self.name} has met their early end. Good luck next time.")
 
-#Define the Monster class            
+#Define the Player Class
+class P_Class:
+    def __init__ (self, name, base_attack, base_defense, items = []):
+
+#Define the Monster class
 class Monster:
     def __init__ (self, name, hp, attack, defense):
         self.name = name
@@ -42,6 +63,11 @@ class Monster:
         self.max_hp = hp
         self.attack = attack
         self.defense = defense
+        self.gold = 0
+        self.loot = []
+    
+    def __repr__(self):
+        return (f"This is a {self.name} with {self.hp}/{self.max_hp}. Its attack strength is {self.attack} and its base defense is {self.defense}.")
 
 #Define the Item class
 class Item:
@@ -50,31 +76,22 @@ class Item:
         self.purpose = purpose
         self.modifier = modifier
 
-#Define the Dungeon class. (Should the dungeon be a class? I'm not sure.)
-class Dungeon:
-    def __init__ (self, length = 5):
-        self.length = length
-
 #Introduction to Game and Player Creation
 print("Greetings, and well met! Welcome to DUNGEON CRAWL. Your task? Survive.")
 
 pc_name = input("First, however, we will need to know a bit more about you. What is your name?\n")
 
-print("Excellent. Welcome to Eosterra, {}.".format(pc_name))
+print(f"Excellent. Welcome to Eosterra, {pc_name}.")
 
 while True:
     pc_class = input("Now, what class will you choose? Choose '1' for Fighter or '2' for Sorcerer.\n")
-    if pc_class == 1:
+    if pc_class == "1":
         pc_class = "Fighter"
-        print("Very well. {} shall be a fighter. Their stats and inventory have been updated to reflect this.")
+        print(f"Very well. {pc_name} shall be a fighter. Their stats and inventory have been updated to reflect this.")
         break
-    elif pc_class == 2:
+    elif pc_class == "2":
         print("I'm sorry, but sorcerers haven't been invented yet. Please choose another class.")
-        continue
     else:
         print("I'm sorry, I didn't catch that. Try again.")
-        continue
-    
-print("PAUSE HERE.")
+        
 
-#STOPPED HERE at 12:05am on 2/28/25
